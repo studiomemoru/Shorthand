b4322bfaf020f693ab96655a54c6d9c2e65b8285
studiomemoru <github@studiomemoru.com>
2023-09-16 04:27:30 +0900

    Fix source code format.

--- $H.js
+++ $H.js
@@ -1,13 +1,13 @@
 /* $horthand.js */
 
 var $H = function(id) {
-	return window.document.getElementById(id)
+  return window.document.getElementById(id);
 }
 
 $H.isset = function(obj) {
-  return !(obj === null || typeof obj === "undefined")
+  return !(obj === null || typeof obj === "undefined");
 }
 
 $H.later = function(func) {
-	setTimeout(func, 0)
+  setTimeout(func, 0);
 }
